49b63a545a2a28c49de6d82fb92db0d3e2f73570
Adéla Brabec <ada-bee@users.noreply.github.com>
2026-03-16 16:40:55 +0100

    Update GameStream cask for 0.1.1 release

--- Casks/gamestream.rb
+++ Casks/gamestream.rb
@@ -1,6 +1,6 @@
 cask "gamestream" do
-  version "0.1.0"
-  sha256 "678e11fb44dd27d5ad141406a8085f11accc4c42c2b19ab6abb4dcb6a3487391"
+  version "0.1.1"
+  sha256 "6df297bdb1d0e6bd0c74255b79cf8c6b0176f9de5640020f85d28a0c98d70573"
 
   url "https://github.com/ada-bee/moonlight-swift/releases/download/v#{version}/GameStream-#{version}.dmg"
   name "GameStream"

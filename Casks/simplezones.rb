8cc8cd3323ed0b43bffab7ce8c4443134087ab71
Adéla Brabec <ada-bee@users.noreply.github.com>
2026-03-16 01:20:14 +0100

    add simplezones cask

---
+++ Casks/simplezones.rb
@@ -0,0 +1,10 @@
+cask "simplezones" do
+  version "0.1.0"
+  sha256 "7a3fb0e7d9bf602fbecc0a637cbcc56dd98ef6f7401a0c9a0e4ab291789e6237"
+
+  url "https://github.com/ada-bee/SimpleZones/releases/download/v0.1.0/SimpleZones.app.zip"
+  name "SimpleZones"
+  homepage "https://github.com/ada-bee/SimpleZones"
+
+  app "SimpleZones.app"
+end
